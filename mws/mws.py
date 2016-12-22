aadcaa55f838c0ebd2c4293b2c006dd570bd1281
Mark <ziplokk.mark.sanders@gmail.com>
2016-12-22 09:08:27 -0600

    Removed error handling in make_request.
    Error handling in make_request method was causing issues with debugging and hiding the response content when a request would fail.
    By removing it, the user can now account for any errors and log the response which caused the error.

--- mws/mws.py
+++ mws/mws.py
@@ -186,29 +186,11 @@ class MWS(object):
         headers = {'User-Agent': 'python-amazon-mws/0.0.1 (Language=Python)'}
         headers.update(kwargs.get('extra_headers', {}))
 
+        response = request(method, url, data=kwargs.get('body', ''), headers=headers)
         try:
-            # Some might wonder as to why i don't pass the params dict as the params argument to request.
-            # My answer is, here i have to get the url parsed string of params in order to sign it, so
-            # if i pass the params dict as params to request, request will repeat that step because it will need
-            # to convert the dict to a url parsed string, so why do it twice if i can just pass the full url :).
-            response = request(method, url, data=kwargs.get('body', ''), headers=headers)
-            response.raise_for_status()
-            # When retrieving data from the response object,
-            # be aware that response.content returns the content in bytes while response.text calls
-            # response.content and converts it to unicode.
-            data = response.content
-
-            # I do not check the headers to decide which content structure to server simply because sometimes
-            # Amazon's MWS API returns XML error responses with "text/plain" as the Content-Type.
-            try:
-                parsed_response = DictWrapper(data, extra_data.get("Action") + "Result")
-            except XMLError:
-                parsed_response = DataWrapper(data, response.headers)
-
-        except HTTPError, e:
-            error = MWSError(str(e.response.text))
-            error.response = e.response
-            raise error
+            parsed_response = DictWrapper(response.content, extra_data.get("Action") + "Result")
+        except XMLError:
+            parsed_response = DataWrapper(response.content, response.headers)
 
         # Store the response object in the parsed_response for quick access
         parsed_response.response = response
